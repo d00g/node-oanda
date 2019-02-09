cd7c893641fd153eb23e1ce340ca735cff8633c6
Doug Hemingway <dougdougdoug@gmail.com>
2019-02-09 17:20:46 +1100

    change function to arror function to allow `this` to be in scope

--- core/oanda_request.js
+++ core/oanda_request.js
@@ -40,12 +40,6 @@ fn.prototype = {
       this.options,
       successWrapper(this.successFn));
 
-    // let that = this;
-    // request.on('error', function(err) {
-    //   console.log(that.errorFn);
-    //   that.errorFn(err);
-    // });
-
     request.on('error', (err) => {
       console.log(this.errorFn);
       this.errorFn(err);
